b25c50fb0070455805508393897b27a278e3cfe1
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-03-19 10:59:17 +0200

    1

--- mar/Magazinquere.sql
+++ mar/Magazinquere.sql
@@ -1,6 +1,15 @@
 --Вывести самые дорогие продукты
 
-Select Product.Name , Firm.Name 
+Select Product.Name , Firm.Name , Price.Price
 from Product, Firm,Price
 where Product.FirmID = Firm.ID and
 		Price.ProductID=Product.ID
+group by  Product.Name, Firm.Name , Price.Price
+having Price.Price=(select  max(Price.Price) 
+					from Price,Firm,Product
+					where Product.FirmID = Firm.ID and
+					Price.ProductID=Product.ID
+					group by Firm.Name
+					)
+
+
