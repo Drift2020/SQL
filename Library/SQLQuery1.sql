50a847098879aaf7c5bf7bde875296d6a4d761ef
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-03-20 15:29:28 +0200

    9

--- Library/SQLQuery1.sql
+++ Library/SQLQuery1.sql
@@ -10,13 +10,60 @@ from   Min_pages INNER JOIN Books  INNER JOIN  Press   ON Press.Id = Books.Id_Pr
 --2. Отобразить названия издательств, которые выпустили книги со средним количеством страниц большим 100.
 
 
-3. Вывести общую сумму страниц всех имеющихся в библиотеке книг, выпущенных издательствами BHV и БИНОМ.
-4. Выбрать имена и фамилии всех студентов, которые брали книги в промежутке между 1 Января 2001 года и текущей датой.
-5. Найти всех студентов, кто на данный момент работает с книгой "Реестр Windows 2000" автора Ольга Кокорева.
-6. Отобразить информацию об авторах, средний объем книг которых (в страницах) более 600 страниц.
-7. Отобразить информацию об издательствах, у которых общее коли-чество страниц выпущенных ими книг больше 700.
-8. Отобразить всех посетителей библиотеки (и студентов и препода-вателей) и книги, которые они брали.
-9. Вывести самого популярного автора(ов) среди студентов и количе-ство книг этого автора, взятых в библиотеке.
+
+SELECT Press.Name , sum(Books.Pages)/sum(Books.Quantity)
+from Books inner join Press on Press.Id = Books.Id_Press
+
+group by Press.Name
+Having sum(Books.Pages)/sum(Books.Quantity)>=100
+
+--3. Вывести общую сумму страниц всех имеющихся в библиотеке книг, выпущенных издательствами BHV и БИНОМ.
+
+SELECT Press.Name, sum(Books.Pages)
+from Books inner join Press on Press.Id = Books.Id_Press
+where Press.Name like '%BHV%' or Press.Name like '%БИНОМ%'
+group by Press.Name
+
+
+--4. Выбрать имена и фамилии всех студентов, которые брали книги в промежутке между 1 Января 2001 года и текущей датой.
+SELECT Students.FirstName , Students.LastName
+from Students inner join S_Cards on S_Cards.Id_Student = Students.Id
+where YEAR (S_Cards.DateOut) >= 2001 and YEAR (S_Cards.DateOut) <= 2018
+
+
+
+--5. Найти всех студентов, кто на данный момент работает с книгой "Реестр Windows 2000" автора Ольга Кокорева.
+
+select Students.FirstName , Students.LastName
+from Students inner join S_Cards inner join Books inner join Authors  on Authors.Id = Books.Id_Author on Books.Id = S_Cards.Id_Book  on Students.Id = S_Cards.Id_Student  
+where Authors.FirstName like 'Ольга' and Authors.LastName like 'Кокорева' and
+	Books.Name like 'Реестр Windows 2000'
+
+--6. Отобразить информацию об авторах, средний объем книг которых (в страницах) более 600 страниц.
+
+Select Authors.FirstName,Authors.LastName,AVG(Books.Pages) as 'Cредний объем книг'
+from Authors inner join Books on Books.Id_Author = Authors.Id
+group by Authors.FirstName,Authors.LastName
+Having AVG(Books.Pages) >= 600 
+
+--7. Отобразить информацию об издательствах, у которых общее коли-чество страниц выпущенных ими книг больше 700.
+
+Select Press.Name ,sum(Books.Pages) as 'Cредний объем книг'
+from Press inner join Books on Books.Id_Press = Press.Id
+group by  Press.Name
+Having sum(Books.Pages) >= 700 
+
+--8. Отобразить всех посетителей библиотеки (и студентов и препода-вателей) и книги, которые они брали.
+
+select Students.FirstName +' '+ Students.LastName as 'ФИО', Books.Name
+from Books inner join S_Cards inner join Students on Students.Id = S_Cards.Id_Student on S_Cards.Id_Book = Books.Id
+
+UNION
+
+select Teachers.FirstName +' '+ Teachers.LastName as 'ФИО', Books.Name
+from Books inner join T_Cards inner join Teachers on Teachers.Id = T_Cards.Id_Teacher on T_Cards.Id_Book = Books.Id
+
+--9. Вывести самого популярного автора(ов) среди студентов и количе-ство книг этого автора, взятых в библиотеке.
 10. Вывести самого популярного автора(ов) среди преподавате-лей и количество книг этого автора, взятых в библиотеке.
 11. Вывести самую популярную(ые) тематику(и) среди студентов и преподавателей.
 12. Отобразить количество преподавателей и студентов, посе-тивших библиотеку.
