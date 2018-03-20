dadfb6ca76c255b1eacb0db860b966040ee8c736
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-03-20 21:54:02 +0200

    14 edit

--- Library/SQLQuery1.sql
+++ Library/SQLQuery1.sql
@@ -75,17 +75,153 @@ select Authors.FirstName+' '+Authors.LastName as 'Автор' , count(S_Cards.DateOut
 from S_Cards inner join Books inner join Authors on Authors.Id = Books.Id_Author on Books.Id = S_Cards.Id_Student 
 --where max(Max_book.count)= count(S_Cards.DateOut)
 group by  Authors.FirstName+' '+Authors.LastName
-
+having count(S_Cards.DateOut) = (select max(Max_books.count)
+									from Max_books 
+									)
 --10. Вывести самого популярного автора(ов) среди преподавате-лей и количество книг этого автора, взятых в библиотеке.
-11. Вывести самую популярную(ые) тематику(и) среди студентов и преподавателей.
-12. Отобразить количество преподавателей и студентов, посе-тивших библиотеку.
-13. Если считать общее количество книг в библиотеке за 100%, то необходимо подсчитать, сколько книг (в процентном отношении) брал каждый факультет.
-14. Отобразить самый читающий факультет и самую читающую кафедру.
-15. Показать автора (ов) самых популярных книг среди препода-вателей и студентов.
-16. Отобразить названия самых популярных книг среди препода-
-вателей и студентов.
-17. Показать всех студентов и преподавателей дизайнеров.
-18. Показать всю информацию о студентах и преподавателях,
-бравших книги.
-19. Показать книги, которые брали и преподаватели и студенты.
-20. Показать сколько книг выдал каждый из библиотекарей.
+
+WITH Max_books AS
+(select count(T_Cards.DateOut) as [count]
+								from T_Cards inner join Books inner join Authors on Authors.Id = Books.Id_Author on Books.Id = T_Cards.Id_Teacher
+								group by  Authors.FirstName+' '+Authors.LastName)
+
+select Authors.FirstName+' '+Authors.LastName as 'Автор' , count(T_Cards.DateOut)
+from T_Cards inner join Books inner join Authors on Authors.Id = Books.Id_Author on Books.Id = T_Cards.Id_Teacher 
+--where max(Max_book.count)= count(S_Cards.DateOut)
+group by  Authors.FirstName+' '+Authors.LastName
+having count(T_Cards.DateOut) = (select max(Max_books.count)
+									from Max_books 
+									)
+
+--11. Вывести самую популярную(ые) тематику(и) среди студентов и преподавателей.
+
+
+
+
+
+WITH Max_Themes AS
+(select Count(Themes.Name) as [count]
+								from Themes inner join Books inner join S_Cards inner join Students 
+on Students.Id = S_Cards.Id_Student 
+on S_Cards.Id_Book = Books.Id
+ on Books.Id_Themes = Themes.Id
+ group by Themes.Name), Max_Themes_T AS
+(select Count(Themes.Name) as [count]
+from Themes inner join Books inner join T_Cards inner join Teachers 
+on Teachers.Id = T_Cards.Id_Teacher 
+on T_Cards.Id_Book = Books.Id
+on Books.Id_Themes = Themes.Id
+group by Themes.Name)
+
+
+
+select  Themes.Name,Count(Themes.Name)
+from Themes inner join Books inner join S_Cards inner join Students 
+on Students.Id = S_Cards.Id_Student 
+on S_Cards.Id_Book = Books.Id
+ on Books.Id_Themes = Themes.Id
+ group by Themes.Name
+ having Count(Themes.Name)= (select max(Max_Themes.count)
+									from Max_Themes )
+
+union
+
+
+
+
+select  Themes.Name,Count(Themes.Name)
+from Themes inner join Books inner join T_Cards inner join Teachers 
+on Teachers.Id = T_Cards.Id_Teacher 
+on T_Cards.Id_Book = Books.Id
+ on Books.Id_Themes = Themes.Id
+ group by Themes.Name
+ having Count(Themes.Name)= (select max(Max_Themes_T.count)
+									from Max_Themes_T )
+
+
+
+--12. Отобразить количество преподавателей и студентов, посе-тивших библиотеку.
+
+
+
+
+Select  count(Students.FirstName ),'Студенты'
+From Students inner join S_cards inner join Books
+on 
+Books.Id = S_Cards.Id_book
+on
+Students.Id = S_Cards.Id_student
+
+UNION 
+
+Select count(Teachers.FirstName) , 'Преподаватели'
+From Teachers inner join T_cards  inner join Books
+on 
+Books.Id = T_Cards.Id_book
+on 
+Teachers.Id = T_Cards.Id_Teacher
+
+
+--13. Если считать общее количество книг в библиотеке за 100%, то необходимо подсчитать, сколько книг (в процентном отношении) брал каждый факультет.
+--x=1040*100:800=130%
+select Faculties.Name, count (Books.Name)*100/(select count(Faculties.Name)as count
+											from Faculties inner join Groups
+											inner join Students 
+											inner join S_Cards 
+											inner join Books on
+											Books.Id = S_Cards.Id_Book on
+											S_Cards.Id_Student = Students.Id on
+											Students.Id_Group = Groups.Id on
+											Groups.Id_Faculty = Faculties.Id
+												) 
+from Faculties inner join Groups inner join Students inner join S_Cards inner join Books on
+Books.Id = S_Cards.Id_Book on S_Cards.Id_Student = Students.Id on Students.Id_Group = Groups.Id on
+Groups.Id_Faculty = Faculties.Id
+group by Faculties.Name
+
+--14. Отобразить самый читающий факультет и самую читающую кафедру.
+WITH Faculty_proc as (select count (Books.Name)*100/(select count(Faculties.Name)as count
+														from Faculties inner join Groups
+														inner join Students 
+														inner join S_Cards 
+														inner join Books on
+														Books.Id = S_Cards.Id_Book on
+														S_Cards.Id_Student = Students.Id on
+														Students.Id_Group = Groups.Id on
+														Groups.Id_Faculty = Faculties.Id
+														) as [count]
+from Faculties inner join Groups inner join Students inner join S_Cards inner join Books on
+Books.Id = S_Cards.Id_Book on S_Cards.Id_Student = Students.Id on Students.Id_Group = Groups.Id on
+Groups.Id_Faculty = Faculties.Id
+group by Faculties.Name)
+
+
+WITH Departments_proc as (select count (Books.Name)*100/(select count(Faculties.Name)as count
+														from Faculties inner join Groups
+														inner join Students 
+														inner join S_Cards 
+														inner join Books on
+														Books.Id = S_Cards.Id_Book on
+														S_Cards.Id_Student = Students.Id on
+														Students.Id_Group = Groups.Id on
+														Groups.Id_Faculty = Faculties.Id
+														) as [count]
+from Faculties inner join Groups inner join Students inner join S_Cards inner join Books on
+Books.Id = S_Cards.Id_Book on S_Cards.Id_Student = Students.Id on Students.Id_Group = Groups.Id on
+Groups.Id_Faculty = Faculties.Id
+group by Faculties.Name)
+
+
+
+
+--15. Показать автора (ов) самых популярных книг среди препода-вателей и студентов.
+
+--16. Отобразить названия самых популярных книг среди препода-вателей и студентов.
+
+--17. Показать всех студентов и преподавателей дизайнеров.
+
+--18. Показать всю информацию о студентах и преподавателях,бравших книги.
+
+--19. Показать книги, которые брали и преподаватели и студенты.
+
+--20. Показать сколько книг выдал каждый из библиотекарей.
