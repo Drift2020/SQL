e61378872d4b7ca6871caf89a3c9528ae2c11a3f
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-03-22 14:52:15 +0200

    ы

--- tur/SQLQueryTure.sql
+++ tur/SQLQueryTure.sql
@@ -1,2 +1,80 @@
-Select (Price_for_Hotels.Price+Price_for_Transport.Price+Price_for_Types_of_rest.Price) as [Сумма тура]
-from 
+
+--сумма за транспорт тура
+WITH Transport_price as (
+Select Tour.ID, sum(Price_for_Transport.Price) as [Count]
+from Tour inner join Tour_Resorts_link 
+inner join Resorts 
+inner join Types_of_transport 
+inner join Price_for_Transport on
+Price_for_Transport.ID= Types_of_transport.MoneyID on 
+Types_of_transport.ID = Resorts.TransportID on 
+Resorts.ID = Tour_Resorts_link.ResortsID on 
+Tour_Resorts_link.TourID = Tour.ID
+group by Tour.ID
+), Hotel_price as (
+									Select Tour.ID , sum(Price_for_Hotels.Price) as [Count]
+									from Tour inner join Tour_Resorts_link 
+									inner join Resorts 
+									inner join Hotels_Resorts_link 
+									inner join Hotels 
+									inner join Time_of_Hotel 
+									inner join Price_for_Hotels on
+									Price_for_Hotels.ID= Time_of_Hotel.MoneyID on
+									Time_of_Hotel.ID = Hotels.TimeID on 
+									Hotels.ID = Hotels_Resorts_link.HotelsID on
+									Hotels_Resorts_link.ResortsID = Resorts.ID on
+									Resorts.ID = Tour_Resorts_link.ResortsID on
+									Tour_Resorts_link.TourID= Tour.ID
+									group by Tour.ID
+									),
+Rest_Price as (
+				Select Tour.ID , sum(Price_for_Types_of_rest.Price) as [Count]
+									from Tour inner join Tour_Resorts_link 
+									inner join Resorts 
+									inner join Types_of_rest_Resortsl_Link 
+									inner join Types_of_rest 
+									inner join Time_of_Types_of_rest 
+									inner join Price_for_Types_of_rest on
+									Price_for_Types_of_rest.ID= Time_of_Types_of_rest.MoneyID on
+									Time_of_Types_of_rest.ID = Types_of_rest.TimeID on 
+									Types_of_rest.ID = Types_of_rest_Resortsl_Link.Types_of_restID on
+									Types_of_rest_Resortsl_Link.ResortsID = Resorts.ID on
+									Resorts.ID = Tour_Resorts_link.ResortsID on
+									Tour_Resorts_link.TourID= Tour.ID
+									group by Tour.ID
+				)
+
+select Tour.Name as 'Название тура', (Transport_price.count+Hotel_price.count+Rest_Price.count) as 'Цена тура($)'
+from Tour inner join Transport_price inner join Hotel_price inner join Rest_Price on 
+Rest_Price.ID=Hotel_price.ID on 
+Hotel_price.ID = Transport_price.ID on
+Transport_price.ID = Tour.ID
+
+--В каких странах и городах будет проходить тур
+Select Tour.Name as 'Тур', Country.Name as'Страна', City.Name as 'Город'
+from Tour inner join Tour_Resorts_link 
+inner join Resorts 
+inner join Country 
+inner join Country_City_link
+inner join City on 
+City.ID = Country_City_link.CityID on
+Country.ID = Country_City_link.CountryID on
+Country.ID = Resorts.CountryID on
+Resorts.ID = Tour_Resorts_link.ResortsID on
+Tour_Resorts_link.TourID = Tour.ID
+
+--Какие виды отдыха в каких городах будут
+
+
+
+Select Tour.Name as 'Тур', Country.Name as'Страна', City.Name as 'Город'
+from Tour inner join Tour_Resorts_link 
+inner join Resorts 
+inner join Country 
+inner join Country_City_link
+inner join City on 
+City.ID = Country_City_link.CityID on
+Country.ID = Country_City_link.CountryID on
+Country.ID = Resorts.CountryID on
+Resorts.ID = Tour_Resorts_link.ResortsID on
+Tour_Resorts_link.TourID = Tour.ID
