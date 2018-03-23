52da5c658fdb5fa3fa3001576cc21f222183b5ec
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-03-23 16:12:04 +0200

    fd

--- teachers/Kill_me.sql
+++ teachers/Kill_me.sql
@@ -4,5 +4,7 @@ use Teachers
 create table FACULTY(
 id int not null identity(1,1) primary key,
 Name varchar(30) not null unique,
-
+Dean varchar(20) unique,
+Building char(5),
+Fund numeric(7,2) 
 )
