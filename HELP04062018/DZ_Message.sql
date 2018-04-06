d19dcae0d7374c4d9989105fcf4906f63f52a205
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-04-06 17:51:49 +0300

    ed

--- HELP04062018/DZ_Message.sql
+++ HELP04062018/DZ_Message.sql
@@ -8,14 +8,18 @@
 
 declare @rate int
 
-select anketa_rate as [rate]
+select anketa_rate.id_kogo ,AVG(anketa_rate.rating)
 from  anketa_rate
+group by anketa_rate.id_kogo  
+order by 1 desc
 
 
 select u.user_id, u.nick, u.age, gender.name , anketa_rate.rating
 
 from  anketa_rate inner join users as u inner join gender on gender.id = u.sex on anketa_rate.id_kogo= u.user_id 
-
+ 
+ select*
+ from sys.systypes
 
 create type mytable as table (id int ,[sum] int)
 declare @My_table mytable
@@ -23,12 +27,12 @@ declare @My_table mytable
 --insert into t2
 --values(250)
 --select * from t2
-declare my_rate cursor for select anketa_rate.rating,anketa_rate.id_kogo from anketa_rate -- объ€вл€ем курсор
+declare my_rate cursor scroll  for select anketa_rate.rating,anketa_rate.id_kogo from anketa_rate -- объ€вл€ем курсор
 open my_rate -- открываем курсор, выполн€€ запрос, указанный в курсоре
 declare @id int, @sum int 
 fetch next from my_rate into @sum, @id -- забираем первую запись
 
-declare my_rate_y cursor for select anketa_rate.rating,anketa_rate.id_kogo from anketa_rate -- объ€вл€ем курсор
+declare my_rate_y cursor scroll  for select anketa_rate.rating,anketa_rate.id_kogo from anketa_rate -- объ€вл€ем курсор
 open my_rate_y -- открываем курсор, выполн€€ запрос, указанный в курсоре
 declare @id_y int, @sum_y int 
 fetch next from my_rate_y into @sum_y, @id_y -- забираем первую запись
@@ -38,13 +42,13 @@ declare @end_sum int , @count int
 while @@FETCH_STATUS = 0
 begin
 
-set @end_sum = @end_sum + @id_y
+set @end_sum = @end_sum + @sum_y
 
 	while @@FETCH_STATUS = 0
 	begin
 		if(@id_y=@id)
 		begin
-		set @end_sum = @end_sum + @id
+		set @end_sum = @end_sum + @sum
 		set @count = @count + 1
 			--raiserror('%d-%d', 0, 25, @sum,@id)
 		end
@@ -53,19 +57,21 @@ set @end_sum = @end_sum + @id_y
 	end
 	fetch first from my_rate into @sum, @id
 	 set @end_sum =  @end_sum /@count
-	 insert into @My_table
-	 values(@end_sum,@id_y)
-  
+	 insert  @My_table (id,[sum])
+	values(@end_sum,@id_y)
+
 
 	fetch next from my_rate_y into @sum_y, @id_y
 	set @end_sum=0
-	set @count = 0
+	set @count = 1
 end
 close my_rate -- закрываем курсор
 deallocate my_rate -- удал€ем пам€ть, выделенную под курсор
 close my_rate_y -- закрываем курсор
 deallocate my_rate_y -- удал€ем пам€ть, выделенную под курсор
 
+select *
+from @My_table
 --2. ѕоказать всех пользователей с высшим образованием, которые не кур€т, не пьют и не употребл€ют наркотики.
 
 
