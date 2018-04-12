a5bf178ed1a3c2a82e19ff4825428c2921cbb761
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-04-12 11:24:49 +0300

    s

--- DEL TABL 100KB/delTab100kb.sql
+++ DEL TABL 100KB/delTab100kb.sql
@@ -1,5 +1,50 @@
+--Показать список всех таблиц определённой базы данных,
+--количество записей в каждой из таблиц,
+--и общий размер каждой таблицы в байтах.
 
-declare @numner int =0, @status int=3, @STR1 nvarchar(max);
+--Удалить все таблицы:
+-- размер которых меньше 100КБ
+-- либо в которых нет записей
+----------------------------------------
+
+
+
+declare  @table table(name nvarchar(200), schemaName nvarchar(200),row int,totalKB int);
+
+insert @table
+SELECT  t.NAME AS TableName, s.Name AS SchemaName, p.rows AS RowCounts, 
+SUM(a.total_pages) * 8 AS TotalSpaceKB
+FROM sys.tables t INNER JOIN      
+     sys.indexes i ON t.OBJECT_ID = i.object_id INNER JOIN 
+     sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id INNER JOIN 
+     sys.allocation_units a ON p.partition_id = a.container_id LEFT OUTER JOIN 
+     sys.schemas s ON t.schema_id = s.schema_id
+WHERE 
+    t.NAME NOT LIKE 'dt%' 
+    AND t.is_ms_shipped = 0
+    AND i.OBJECT_ID > 255 
+GROUP BY t.Name, s.Name, p.Rows
+ORDER BY t.Name
+
+
+while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+declare  @STR1 nvarchar(max);
 declare gc cursor for select name from  sys.master_files   -- объявляем курсор
 open gc -- открываем курсор, выполняя запрос, указанный в курсоре
 
@@ -19,9 +64,10 @@ fetch next from sizes into @si
 while @@FETCH_STATUS = 0 
 begin
 
-if((@numner%4=0and @gname!='master' and @status=1 )
- or (@gname!='master' and @status=2 and (CHARINDEX('a', @gname)!= 0 or CHARINDEX('A', @gname)!=0 )) 
-or ( @si*1.0/128 < 1 and @gname!='master' and @status=3))
+
+raiserror('%d-%s', 0, 25, @si,@gname)
+
+if ( @si*1.0/128 < 1 and @gname!='master')
 begin
 
 	print @gname -- выводим значение временной переменной
@@ -33,7 +79,7 @@ begin
 end
 fetch next from sizes into @si
 fetch next from gc into @gname	
-set @numner= @numner+1
+
 end
 
 close gc 
