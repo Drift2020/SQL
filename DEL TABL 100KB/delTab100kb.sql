6d5adc02080d575efefaa81a80bae5cde0fd779e
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-04-13 09:51:11 +0300

    ss

--- DEL TABL 100KB/delTab100kb.sql
+++ DEL TABL 100KB/delTab100kb.sql
@@ -31,20 +31,34 @@ ORDER BY t.Name
 
 
 
+declare [table_my] cursor for select name,row,totalKB from  @table   -- объявляем курсор
+open [table_my] -- открываем курсор, выполняя запрос, указанный в курсоре
 
-declare name nvarchar(200), schemaName nvarchar(200),row int,totalKB int)
-declare sizes cursor for select size from sys.master_files 
-open sizes
-fetch next from sizes into @si
 
+declare @name nvarchar(200),@row int,@totalKB int
+fetch next from [table_my] into @name,@row,@totalKB
+declare @statement nvarchar(200)
 
 while @@FETCH_STATUS = 0 
 begin
-print ('hi')
 
-set @i=@i+1
+	if @totalKB<100
+		begin
+			raiserror('%s-%d-%d-DELETE', 0, 25, @name,@row,@totalKB)
+
+			--set @statement = 'DROP TABLE ' +@name
+			--exec sp_executesql @statement
+
+		end
+	else
+		begin 
+			raiserror('%s-%d-%d', 0, 25, @name,@row,@totalKB)
+		end
+	fetch next from [table_my] into @name,@row,@totalKB
 end
 
+close [table_my]-- закрываем курсор
+deallocate [table_my] -- удаляем память, выделенную под курсор
 
 
 
@@ -59,47 +73,44 @@ end
 
 
 
+--declare  @STR1 nvarchar(max);
+--declare gc cursor for select name from  sys.master_files   -- объявляем курсор
+--open gc -- открываем курсор, выполняя запрос, указанный в курсоре
 
-declare  @STR1 nvarchar(max);
-declare gc cursor for select name from  sys.master_files   -- объявляем курсор
-open gc -- открываем курсор, выполняя запрос, указанный в курсоре
-
-declare @gname nvarchar(50) -- создаем временную переменную для хранения результатов
- -- забираем первую запись
-
+--declare @gname nvarchar(50)
  
--- SELECT name, size*1.0/128 AS[Size in MBs]  
---FROM sys.master_files 
+---- SELECT name, size*1.0/128 AS[Size in MBs]  
+----FROM sys.master_files 
 
-declare @si int
-declare sizes cursor for select size from sys.master_files 
-open sizes
-fetch next from sizes into @si
-
-
-while @@FETCH_STATUS = 0 
-begin
+--declare @si int
+--declare sizes cursor for select size from sys.master_files 
+--open sizes
+--fetch next from sizes into @si
 
 
-raiserror('%d-%s', 0, 25, @si,@gname)
+--while @@FETCH_STATUS = 0 
+--begin
 
-if ( @si*1.0/128 < 1 and @gname!='master')
-begin
 
-	print @gname -- выводим значение временной переменной
+--raiserror('%d-%s', 0, 25, @si,@gname)
+
+--if ( @si*1.0/128 < 1 and @gname!='master')
+--begin
+
+--	print @gname -- выводим значение временной переменной
 	
-	-- забираем следующую запись
-	--set @STR1= 'Drop database '+@gname
-	--exec sp_sqlexec @gname
+--	-- забираем следующую запись
+--	--set @STR1= 'Drop database '+@gname
+--	--exec sp_sqlexec @gname
 
-end
-fetch next from sizes into @si
-fetch next from gc into @gname	
+--end
+--fetch next from sizes into @si
+--fetch next from gc into @gname	
 
-end
+--end
 
-close gc 
-close sizes-- закрываем курсор
-deallocate gc -- удаляем память, выделенную под курсор
-deallocate sizes
+--close gc 
+--close sizes-- закрываем курсор
+--deallocate gc -- удаляем память, выделенную под курсор
+--deallocate sizes
 
