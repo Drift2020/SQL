b1b512ecde59a067fd020255365f6fc5b52bdd68
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-04-13 17:45:17 +0300

    ss

--- stroki/SQLQuery1.sql
+++ stroki/SQLQuery1.sql
@@ -25,9 +25,40 @@ set @symbvol = 'f'
 
 --2. Подсчитать количество слов во введённом предложении.
 
+declare @str1 nvarchar(200), @symbvol1 nvarchar(200)
 
+set @str1 = 'asdfghjf dfgdfg dfgdfg dfgdfg gdfg'
+set @symbvol1 = ' '
+
+  declare @i1 int =-1, @count1 int =0
+  
+
+
+  while  @i1 <> 0  
+  begin
+   
+    set @i1 = CHARINDEX(@symbvol1,@str1,@i1+1)
+	if(@i1 <> 0)
+	 begin
+
+	  set @count1=@count1+1
+
+
+	 end 
+  end
+   set @count1=@count1+1
+  raiserror('count - %d', 0, 25,@count1)
 
 --3. Определить, является ли строка палиндромом. Примеры палиндромов:
+declare @str2 nvarchar(200) = 'Нажал кабан на баклажан',@str3 nvarchar(200)
+SET @str2 = REPLACE(@str2, ' ', '');
+
+set @str2=LOWER(@str2)
+set @str3 = reverse(@str2)
+if(@str2=@str3)
+print('палиндром')
+else
+print('nooo палиндром')
 
 --Нажал кабан на баклажан.
 --Я так нежен, Катя.
@@ -35,7 +66,33 @@ set @symbvol = 'f'
 
 --4. Посчитать, сколько раз заданное слово содержится в строке.
 
+declare @str5 nvarchar(200), @symbvol5 nvarchar(200)
+set @str5 = 'Nurrunse, I spy gypsies run'
+set @symbvol5 = 'run'
+
+  declare @i5 int =-1, @count5 int =0
+  
+
+
+  while  @i5 <> 0
+  begin
+   set @i5 = CHARINDEX(@symbvol5,@str5,@i5+1)
+	if(@i5 <> 0 and (CHARINDEX(@symbvol5,@str5,@i5+len(@str5)) <> 0 or CHARINDEX(@symbvol5,@str5,@i5-1) <> 0))
+	 begin
+
+	  set @count5=@count5+1
+
+	 end 
+  end
+
+  raiserror('count - %d', 0, 25,@count5)
+
+
 --5. Проверить, содержит ли заданная строка слова 'viagra' или 'XXX'.
+
+
+
+
 --Регистр не учитывать.
 --Если содержит - пишем на экране "это спам".
 --Если нет - пишем "это не спам".
