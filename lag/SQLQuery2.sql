1d1acd4d7ee3e4d63f31f728622d77176ebabf42
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-04-16 11:45:43 +0300

    s

--- lag/SQLQuery2.sql
+++ lag/SQLQuery2.sql
@@ -30,11 +30,33 @@ CREATE PROCEDURE ProductSummary @n int, @a int ,@b int, @c int AS
 BEGIN
 
 	if @a=@b and @b=@c
-	raiserror('рівностороння #%s', 0, 25,@n)
+	raiserror('рівностороння #%d', 0, 25,@n)
 	else if @a=@b or @b=@c or @c=@a
-	raiserror('Двостороння #%s', 0, 25,@n)
+	raiserror('Двостороння #%d', 0, 25,@n)
 	else if @a != @b and @b != @c and @c != @a  
-	and 
-	raiserror('Скаліне #%s', 0, 25,@n)
+	and @a < @b*2 and @a < @c*2 
+	and @b < @c*2 and @b < @a*2 
+	and @c < @a*2 and @c < @b*2 
+	raiserror('Скаліне #%d', 0, 25,@n)
+	else
+	raiserror('Не трикутник #%d', 0, 25,@n)
 
 END;
+go
+
+declare [table_my] cursor for select id,side1,side2,side3 from Table_1   -- объявляем курсор
+open [table_my] 
+
+declare @id int,@side1 int,@side2 int,@side3 int
+fetch next from [table_my] into @id, @side1, @side2, @side3
+
+while @@FETCH_STATUS = 0 
+begin
+
+exec ProductSummary @id, @side1, @side2, @side3
+fetch next from [table_my] into @id, @side1, @side2, @side3
+
+end
+
+close [table_my]-- закрываем курсор
+deallocate [table_my]
