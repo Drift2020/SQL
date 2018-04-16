cd8c8716e87bfd0bbc67b78b85a9b6caf65415f0
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-04-16 10:07:56 +0300

    s

--- stroki/SQLQuery1.sql
+++ stroki/SQLQuery1.sql
@@ -70,13 +70,17 @@ declare @str5 nvarchar(200), @symbvol5 nvarchar(200)
 set @str5 = 'Nurse, I spy gypsies run'
 set @symbvol5 = 'run'
 
-  declare @i5 int =-1, @count5 int =0
-  
+
+  declare @i5 int =-1, @count5 int =0,@iPS nvarchar(200),@iPE nvarchar(200) 
+  set @iPS = @symbvol5 + ' '
+  set @iPE =' ' + @symbvol5 
 
 
   while  @i5 <> 0
   begin
+
    set @i5 = CHARINDEX(@symbvol5,@str5,@i5+1)
+
 	if(@i5 <> 0 )
 	 begin
 
@@ -89,6 +93,51 @@ set @symbvol5 = 'run'
 
 
 --5. Проверить, содержит ли заданная строка слова 'viagra' или 'XXX'.
+
+
+create database [kill]--пифагор
+go
+use [kill]
+go
+create table mat(
+ID int not null primary key IDENTITY(1,1),
+[str] nvarchar(255)
+)
+go
+
+
+insert into mat ([str]) values ('viagra')
+insert into mat ([str]) values ('xxx')
+go 
+
+declare @str_mat nvarchar(255)='buy ViAgRA now'
+
+declare [table_my] cursor for select ID,[str] from  mat   -- объявляем курсор
+open [table_my] 
+
+
+declare @name nvarchar(200),@id int, @is_ bit = 1
+fetch next from [table_my] into @id, @name
+
+
+while @@FETCH_STATUS = 0 
+begin
+
+	if CHARINDEX(LOWER(@str_mat),@name,0)<>0
+		begin
+			print('Spam')
+			set @is_=0
+		end
+	
+
+	fetch next from [table_my] into @id,@name
+end
+
+if @is_ = 1
+raiserror('Not spam', 0, 25, @name,@id)
+
+close [table_my]-- закрываем курсор
+deallocate [table_my] -- удаляем память, выделенную под курсор
 
 
 
